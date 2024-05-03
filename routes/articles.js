6ce82f09f5cac55103a9155785ac3bffae866447
SatyakiB7076 <satyakit1998@gmail.com>
2024-05-03 14:42:26 +0530

    working on database schemas

--- routes/articles.js
+++ routes/articles.js
@@ -1,14 +1,29 @@
 //all routes directly related to the articles will be created here
 
 const express =require('express')
+const Article = require('../models/article')
+
 const router=express.Router()
 
 //here whatever relative path we are giving it will be after /articles/something like that
 router.get('/new',(req,res)=>{
     res.render('articles/new')
 })
-router.post('/',(req,res)=>{
-    
+router.get("/:id",(req,res)=>{
+
+})
+router.post('/', async(req,res)=>{
+    const article=new Article({
+        title:req.body.title,
+        description:req.body.description,
+        markdown:req.body.markdown
+    })
+    try{
+        article =await article.save()
+res.redirect(`/articles/${article.id}`)
+    }catch(e){
+res.render('articles/new', {article})
+    }
 })
 
 module.exports=router
--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const app = express();
 const articleRouter = require("./routes/articles");
+const mongoose =require('mongoose')
 
-//now anything we create using articleRouter will be added after /articles
-app.use("/articles", articleRouter);
 
 app.set("view engine", "ejs");
+//datatbase connection
+mongoose.connect('mongodb://127.0.0.1:27017/blog')
+//now anything we create using articleRouter will be added after /articles
+app.use("/articles", articleRouter);
+app.use(express.urlencoded({extended: false}))
 app.get("/", (req, res) => {
   const articles = [
     {
       title: "Test Article",
       createdAt: new Date(),
       description: "Test Description",
-    },{
-        title: "Test Article 2",
-        createdAt: new Date(),
-        description: "Test Description 2",
-      }
+    },
+    {
+      title: "Test Article 2",
+      createdAt: new Date(),
+      description: "Test Description 2",
+    },
   ];
   //provide the relative path for the index.ejs file
   res.render("articles/index.ejs", { articles });
